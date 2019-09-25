82e58394b205f478d8db88e7db4b442f2b33b64f
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-09-25 16:18:26 -0700

    Working through import exports

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,5 @@
-console.log("hello GraphQL!");
+import myCurrentLocation, { message, name } from "./myModule";
+
+console.log(message);
+console.log(name);
+console.log(myCurrentLocation);
---
+++ graphql-basics/src/myModule.js
@@ -0,0 +1,7 @@
+const message = "Some message from myModule.js";
+
+const name = "Alexander";
+
+const location = "California";
+
+export { message, name, location as default };
